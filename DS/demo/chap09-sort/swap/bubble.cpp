6409efddc176fa3bb682f9ed8d138c2938fc0e39
Erewhon-Lucy <1663523296@qq.com>
2020-06-06 10:39:55 +0800

    42020.6.6

---
+++ DS/demo/chap09-sort/swap/bubble.cpp
@@ -0,0 +1,19 @@
+#include "../record.h"
+
+void BubbleSort(RecordType r[], int length)
+/*对记录数组r做冒泡排序，length为数组的长度*/
+{
+    int i, j;
+    RecordType x;
+
+    for (i = 1; i <= length - 1; ++i)
+    {
+        for (j = 1; j <= length - i; ++j)
+            if (r[j + 1].key < r[j].key)
+            {
+                x = r[j];
+                r[j] = r[j + 1];
+                r[j + 1] = x;
+            }
+    }
+} /*  BubbleSort  */
